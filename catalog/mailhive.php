2bbedf1e2c2f246df2dd3bf2793ead19d6455a95
cord <cf@lct.de>
2015-04-22 15:55:23 +0200

    fall back to /mailhive if exists

--- catalog/mailhive.php
+++ catalog/mailhive.php
@@ -17,7 +17,7 @@
 if (!defined('MH_ROOT_PATH')) {
     // default location
     $_MH_ROOT_PATH = 'mailhive/';
-    $_mh_search_paths = array('ext/mailhive/', 'includes/external/mailhive/');
+    $_mh_search_paths = array('mailhive/', 'ext/mailhive/', 'includes/external/mailhive/');
 
     foreach ($_mh_search_paths as $_MH_ROOT_PATH_TRY) {
         if (file_exists($_MH_ROOT_PATH_TRY . 'cloudbeez/cloudloader_core.php')) {
